e617a9f078aa387639ec57eef0b82e6c994e1c6f
Maria Tereza <maharaujo002@gmail.com>
2025-03-21 22:45:18 -0300

    feat: funcionalidades de atualizaçao e exclusao das techs

--- src/stores/useDataTechStore.ts
+++ src/stores/useDataTechStore.ts
@@ -7,18 +7,22 @@ interface User {
   email: string;
   token: string;
 }
-interface ITechs { 
-  title: string,
-  status: string 
-}; 
+
+interface ITechs {
+  id: string;
+  title: string;
+  status: string;
+}
 
 interface AuthState {
   user: User | null;
   loginAuth: (dataForm: { email: string; password: string }, navigate: (path: string) => void) => Promise<void>;
   registerUser: (dataForm: { email: string; password: string; name: string }, navigate: (path: string) => void) => Promise<void>;
   loadData: (setLoading: (loading: boolean) => void, navigate: (path: string) => void) => Promise<void>;
-  addTech: (dataForm: { title: string; status: string }) => Promise<void>;
-  techsData: ITechs[],
+  addTech: (dataForm: ITechs) => Promise<void>;
+  techsData: ITechs[];
+  deleteTech: (techId: string) => Promise<void>;
+  updateTech: (dataForm: ITechs, editId: string) => Promise<void>;
 }
 
 export const useDataTechStore = create<AuthState>((set) => ({
@@ -50,8 +54,7 @@ export const useDataTechStore = create<AuthState>((set) => ({
   },
 
   loadData: async (setLoading, navigate) => {
-    const token = JSON.parse(localStorage.getItem("@TOKEN") || "null")
-  
+    const token = JSON.parse(localStorage.getItem("@TOKEN") || "null");
 
     if (token) {
       try {
@@ -61,7 +64,7 @@ export const useDataTechStore = create<AuthState>((set) => ({
             Authorization: `Bearer ${token}`,
           },
         });
-        set({ techsData: data.techs});
+        set({ techsData: data.techs });
       } catch (error) {
         console.error("Erro ao carregar dados:", error);
         navigate("/login");
@@ -85,6 +88,44 @@ export const useDataTechStore = create<AuthState>((set) => ({
       console.log("Tecnologia adicionada com sucesso!");
     } catch (error) {
       console.error("Erro ao adicionar tecnologia:", error);
+    }
+  },
+
+  deleteTech: async (techId) => {
+    const token = JSON.parse(localStorage.getItem("@TOKEN") || "null");
+    try {
+      await api.delete(`/users/techs/${techId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      set((state) => ({
+        techsData: state.techsData.filter((tech) => tech.id !== techId),
+      }));
+      console.log("Tecnologia excluída com sucesso");
+    } catch (error) {
+      console.log("Falha na exclusão", error.response?.data?.message);
+    }
+  },
+
+  updateTech: async (dataForm, editId) => {
+    const token = JSON.parse(localStorage.getItem("@TOKEN") || "null");
+    try {
+      const response = await api.put<ITechs>(`users/techs/${editId}`, dataForm, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("Tecnologia editada com sucesso!");
+
+      set((state) => ({
+        techsData: state.techsData.map((tech) =>
+          tech.id === editId ? { ...tech, ...response.data } : tech
+        ),
+      }));
+
+    } catch (error) {
+      console.error("Erro ao editar tecnologia:", error);
     }
   },
 }));
